80e2d8e8d80ec6d535f59725531301724dc1dcfa
Moacyr Pereira <moacyrpereira10@hotmail.com>
2022-02-02 11:26:57 -0300

    Projeto completo

--- src/App.js
+++ src/App.js
@@ -6,8 +6,22 @@ import Conversor from "./components/Conversor"
 function App() {
   return (
     <div className="App">
-      
-    <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <h1>Conversor de moedas</h1>
+      <div className="Linha">
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+   
+      <div className="Linha">
+        <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+    
+      <div className="Linha">
+        <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+
 
     </div>
   );
