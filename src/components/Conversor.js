9c496cb8eef1c2fcd635d720edd87eae05909f5b
Moacyr Pereira <moacyrpereira10@hotmail.com>
2022-02-02 09:49:48 -0300

    Projeto Iniciado

---
+++ src/components/Conversor.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Conversor extends Component {
+  render() {
+    return (
+      <div classname="conversor">
+          
+        <h2>USD para BRL</h2>
+        <input type="text"></input>
+        <input type="button"></input>
+        <h2>Valor convertido</h2>
+
+      </div>
+    )
+  }
+}
